1e3a64911b59bb8cf17f6f1894f49f09c5592a50
MadhurToppo <madhur.toppo@gmail.com>
2020-11-12 23:50:49 +0530

    Initial commit

---
+++ src/test/java/com/madhurtoppo/MathUtilsTest.java
@@ -0,0 +1,18 @@
+package com.madhurtoppo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathUtilsTest {
+
+    @Test
+    void addTest() {
+        MathUtils mathUtils = new MathUtils();
+
+        int expected = 7;
+        int actual = mathUtils.add(3, 4);
+
+        assertEquals(expected, actual, "The add method should add two numbers");
+    }
+}
