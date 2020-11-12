11ede8f01a80d671140b6217b97eb414620103d6
MadhurToppo <madhur.toppo@gmail.com>
2020-11-12 23:52:25 +0530

    Formatted code

--- src/test/java/com/madhurtoppo/MathUtilsTest.java
+++ src/test/java/com/madhurtoppo/MathUtilsTest.java
@@ -9,10 +9,8 @@ class MathUtilsTest {
     @Test
     void addTest() {
         MathUtils mathUtils = new MathUtils();
-
         int expected = 7;
         int actual = mathUtils.add(3, 4);
-
         assertEquals(expected, actual, "The add method should add two numbers");
     }
 }
