ff2e52ae0c06f71ee121d0818d29027122eb433e
MadhurToppo <madhur.toppo@gmail.com>
2020-11-13 14:07:48 +0530

    Added unit tests

--- src/main/java/com/madhurtoppo/MathUtils.java
+++ src/main/java/com/madhurtoppo/MathUtils.java
@@ -5,4 +5,21 @@ public class MathUtils {
     public int add(int a, int b) {
         return a + b;
     }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+
+    public double circleArea(double radius) {
+        return Math.PI * radius * radius;
+    }
+
 }
--- src/test/java/com/madhurtoppo/MathUtilsTest.java
+++ src/test/java/com/madhurtoppo/MathUtilsTest.java
@@ -1,16 +1,73 @@
 package com.madhurtoppo;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+
+import java.awt.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class MathUtilsTest {
 
+    MathUtils mathUtils;
+
+    @BeforeAll
+    void beforeAllInit() {
+        System.out.println("---Initializing all---");
+    }
+
+    @BeforeEach
+    void init(TestInfo testInfo) {
+        mathUtils = new MathUtils();
+        System.out.println("Started..." + testInfo.getDisplayName());
+    }
+
+    @Nested
+    class testAdd {
+        @Test
+        @DisplayName("Addition Test for +ve")
+        void testAddPositive() {
+            assertEquals(7, mathUtils.add(3, 4), "The add method should add two numbers");
+        }
+
+        @Test
+        @DisplayName("Addition Test for -ve")
+        void testAddNegative() {
+            assertEquals(-5, mathUtils.add(-2, -3), "The add method should add two numbers");
+        }
+    }
+
     @Test
-    void addTest() {
-        MathUtils mathUtils = new MathUtils();
-        int expected = 7;
-        int actual = mathUtils.add(3, 4);
-        assertEquals(expected, actual, "The add method should add two numbers");
+    @DisplayName("Division Test")
+    @EnabledOnOs(OS.WINDOWS)
+    void testDivide() {
+        assertAll(
+                () -> assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0), "Divide by zero should throw"),
+                () -> assertEquals(2, mathUtils.divide(4, 2))
+        );
+    }
+
+    @RepeatedTest(3)
+    @DisplayName("Circle Area Test")
+    void testCircleArea() {
+        assertEquals(314.1592653589793, mathUtils.circleArea(10), "Should return correct circle area");
+    }
+
+    @AfterEach
+    void tearDown(TestInfo testInfo) {
+        System.out.println("Finished..." + testInfo.getDisplayName());
+    }
+
+    @AfterAll
+    void tearDownAll() {
+        System.out.println("---Tearing down all---");
+    }
+
+//    @Test
+//    @Disabled
+    void testDisabled() {
+        fail("This test is disabled");
     }
 }
