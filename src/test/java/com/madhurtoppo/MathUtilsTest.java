e6819831a569b497625964cb46e788706dd5b025
MadhurToppo <madhur.toppo@gmail.com>
2020-12-16 23:11:34 +0530

    Removed extra divide test

--- src/test/java/com/madhurtoppo/MathUtilsTest.java
+++ src/test/java/com/madhurtoppo/MathUtilsTest.java
@@ -63,7 +63,7 @@ class MathUtilsTest {
 
     @AfterEach
     void tearDown(TestInfo testInfo) {
-        System.out.println("Finished testing..." + testInfo.getDisplayName());
+        System.out.println("Testing finished..." + testInfo.getDisplayName());
     }
 
     @AfterAll
